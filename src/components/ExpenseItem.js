d1cc7aab0e5acf84fa5e697c32cd5596b3e45946
nskrs <nitinkolhe051>
2023-03-20 18:55:56 +0530

    React

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div><h2>Expense item!</h2></div>
+        <div>Food</div>
+        <div>Rs 10</div><br></br>
+<div> Petrol </div>
+<div> Rs 100 </div> <br></br>
+<div>Movies</div>
+<div>Rs 200</div>
+    </div>
+   
+      
+
+    )
+}
+
+export default ExpenseItem;
