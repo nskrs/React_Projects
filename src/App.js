9db57bea0239a80847ec2060cb0d57a1976831ef
nskrs <nitinkolhe051>
2023-03-22 21:19:16 +0530

    task 9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import ExpenseItems from './components/ExpenseItems';
+import ExpenseItem from './components/newexpense'
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'food',
+      amount: 10,
+      date: new Date(2023, 3, 22),
+      LocationOfExpenditure: 'ABC',
+    },
+    { id: 'e2', title: 'petrol', 
+    amount: 100, 
+    date: new Date(2023, 3, 22),
+    LocationOfExpenditure:'HNJ',
+  },
+    {
+      id: 'e3',
+      title: 'movie',
+      amount: 200,
+      date: new Date(2023, 3, 28),
+      LocationOfExpenditure: 'XYZ',
+    },
+    {
+      id: 'e4',
+      title: 'Other',
+      amount: 450,
+      date: new Date(2023, 3, 24),
+      LocationOfExpenditure: 'DEF',
+    },
+  ];
+const newexpense=[];
+  for(let i=0;i<expenses.length;i++)
+  {
+    newexpense.push(<ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} location={expenses[i].LocationOfExpenditure}></ExpenseItem>)
+  }
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItems></ExpenseItems>
+      {newexpense}
     </div>
   );
 }
