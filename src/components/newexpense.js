e1eac214d2283b85e4448b925a2404609c409702
nskrs <nitinkolhe051>
2023-03-23 11:55:16 +0530

    Making Components Smaller and Readable

--- src/components/newexpense.js
+++ src/components/newexpense.js
@@ -1,15 +1,14 @@
-import './newexpense.css';
+import ExpenseDate from './newexdate';
+import './newexpense.css'
 
-function ExpenseItem(props) {
-  return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
+function ExpenseDetails(props){
+    return(
+    <div className='expenses'>
+      < ExpenseDate date ={props.date}/>
+        <div className='expense-item__description'>
+        <h2>{props.title}</h2></div>
+        <div className='expense-item__price'>Rs.{props.amount}</div>
     </div>
-  );
+    )
 }
-
-export default ExpenseItem;
+export default ExpenseDetails;
