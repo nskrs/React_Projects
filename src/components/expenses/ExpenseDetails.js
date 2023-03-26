9173c5d35a2e6d35417a729f2d7d4d7d6b7ce55d
nskrs <nitinkolhe051>
2023-03-26 19:42:56 +0530

    Button change amount

--- src/components/expenses/ExpenseDetails.js
+++ src/components/expenses/ExpenseDetails.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseDetails.css';
 
 
-
-      
+ 
 
 const ExpenseDetails = (props) =>{
+  const [title, setTitle]=useState(props.title);
+  const[amount,setAmount]=useState(props.amount);
+
+  
   const clickHandler=()=>{
-    console.log('clicked!!')
-}
+    setTitle('Updated!');
+    console.log(title);
+};
+
+const changeAmount= () => {
+  setAmount(100);
+  console.log(amount);
+};
+
 
 const deleteExpense=()=>{
    alert("are you sure")
@@ -20,10 +30,11 @@ const deleteExpense=()=>{
   <Card className='expenses'>
     < ExpenseDate date ={props.date}/>
       <div className='expense-item__description'>
-      <h2>{props.title}</h2>
-      <div className='expense-item__price'>Rs.{props.amount}</div>
+      <h2>{title}</h2>
+      <div className='expense-item__price'>Rs.{amount}</div>
       </div>
        <button onClick={clickHandler}> Change Title</button>
+       <button onClick={changeAmount}>Change Amount</button>
       <button onClick={deleteExpense} > Delete Expense</button>   
   </Card>
   );
