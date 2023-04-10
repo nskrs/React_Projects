738fe7fdd154b24e48cb98ba20b20ea4a570a8f0
nskrs <nitinkolhe051>
2023-04-10 11:10:14 +0530

    Admin panel for Medicine shop Sellers

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useState } from 'react';
+import React from 'react'
+import Item from './components/3Hours Project 2/Item';
 
-import Header from './components/Restaurant/Layout/Header';
-import Meals from './components/Restaurant/Meals/Meals';
-import Cart from './components/Restaurant/Cart/Cart'
-import CartProvider from './components/Restaurant/Cart/CartProvider';
-
-function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
+const App = () => {
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
-  );
+    <div>
+      <Item/>
+    </div>
+  )
 }
 
 export default App;
